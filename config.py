524839569720af01da04af6d54a1bbff866c04b6
Poison786 <168622740+Poison786@users.noreply.github.com>
2024-05-04 10:15:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 29707621
-api_hash = "a1f31eeb31a0b9e36f551da6b8b85752"
-bot_token = "6634971116:AAGhU6IQBREjXdliqJr9CGh1ZCLpoYbGP5o"
-auth_users = "6707073614, 1406300051, 6801378994, 870101201"
+api_id = 24932120
+api_hash = "b16b8704b5c839ed24f4ed5824978a3b"
+bot_token = "6786478176:AAHi1DuSAF5eNk0L-ydm2PW7eqndOjoS0Uc"
+auth_users = "7171191819, 6518807195"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6707073614"
+osowner_users = "7045457279"
 owner_users = [int(num) for num in osowner_users.split(",")]
