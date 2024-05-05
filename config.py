911ca6ce891f63ce00feb5ae6845e9151e9e90fe
Poison786 <168622740+Poison786@users.noreply.github.com>
2024-05-05 06:42:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id = 24932120
 api_hash = "b16b8704b5c839ed24f4ed5824978a3b"
 bot_token = "6786478176:AAHi1DuSAF5eNk0L-ydm2PW7eqndOjoS0Uc"
-auth_users = "7171191819, 6518807195"
+auth_users = "7171191819, 6518807195,7045457279"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "7045457279"
 owner_users = [int(num) for num in osowner_users.split(",")]
